371aa2bb020f37a6bab904fba466c0ca77b559b1
lorenz21 <lorenz21@hawaii.edu>
2016-09-25 17:24:51 -1000

    created transform functions for CSS
    Nav links change the background image

---
+++ functions.js
@@ -0,0 +1,15 @@
+function earthTransform() {
+  document.getElementById("gonnaChange").style.backgroundImage = "url(\"images/earth2.jpg\")";
+}
+
+function waterTransform() {
+  document.getElementById("gonnaChange").style.backgroundImage = "url(\"images/water.jpg\")";
+}
+
+function airTransform() {
+  document.getElementById("gonnaChange").style.backgroundImage = "url(\"images/air3.jpg\")";
+}
+
+function fireTransform() {
+  document.getElementById("gonnaChange").style.backgroundImage = "url(\"images/fire3.jpg\")";
+}
